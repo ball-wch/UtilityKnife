4623467b3ac03736add683ca7a4c8ebbc7dc81af
ball-wch <worachotn@gmail.com>
2022-01-26 00:38:01 +0700

    Add function

--- Calculator.cs
+++ Calculator.cs
@@ -2,9 +2,25 @@ namespace UtilityKnife.Calculator
 {
     public class Calculator
     {
-        public static int Add (int x, int y)
+        public static double Add(double x, double y)
         {
             return x + y;
+        }
+        public static double Subtract(double x, double y)
+        {
+            return = x - y;
+        }
+        public static double Multilply(double x, double y)
+        {
+            return x * y;
+        }
+        public static int Division(int x, int y)
+        {
+            return x / y
+        }
+        public static double Division(double x, double y)
+        {
+            return x / y;
         }
     }
 }
