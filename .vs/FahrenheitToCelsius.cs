75c93e32f81a856df996f06cd26712bcb4cf0f0e
ball-wch <worachotn@gmail.com>
2022-01-26 00:04:48 +0700

    Create skeleton for FtoC converter

---
+++ .vs/FahrenheitToCelsius.cs
@@ -0,0 +1,12 @@
+namespace UtiliyKnife.Converters
+{
+    public class FahrenheitToCelsius
+    {
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahrenheitTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsiusl
+        }
+    }
+}
